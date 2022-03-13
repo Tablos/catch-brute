61889222a77c6dd8c00b464464a7e7571aeb0b59
Tablos <100886073+Tablos@users.noreply.github.com>
2022-03-13 10:25:52 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-apt-get install python3 python3-pip -y
-pip3 install -r requirements.txt
-chmod 777 catch
-cp catch /bin
-mkdir  /home/.tablos/
-mkdir /home/.tablos/catch-brute
-cp * /home/.tablos/catch-brute
-cat se 
