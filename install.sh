52918c116a6adde447e0527d9ad642f23f0e0112
Tablos <100886073+Tablos@users.noreply.github.com>
2022-03-13 10:29:00 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+apt-get install python3 python3-pip -y
+pip3 install -r requirements.txt
+chmod 777 catch
+cp catch /bin
+mkdir  /home/.tablos/
+mkdir /home/.tablos/catch-brute
+cp * /home/.tablos/catch-brute
+clear
+cat se0
+cat se 
